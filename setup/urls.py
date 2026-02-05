2f38a825ef0e7f2f58af1ee5c849d936693b8e3d
milinull <raphalol177@gmail.com>
2026-02-05 18:35:54 -0300

    att

--- setup/urls.py
+++ setup/urls.py
@@ -12,7 +12,7 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
     openapi.Info(
         title="API Case 2 - Processos de Tratamento de dados",
-        default_version='v1',
+        default_version="v1",
         description="Documentação da API Case 2",
     ),
     public=True,
@@ -22,14 +22,19 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("case2.urls")),
-
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-# Documentação Swagger
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-# Documentação ReDoc
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Documentação Swagger
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    # Documentação ReDoc
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
